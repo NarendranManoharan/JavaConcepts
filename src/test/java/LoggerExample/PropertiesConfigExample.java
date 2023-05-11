4bdf1885e6f50238d13611084cf021e4dc8c0674
narman <narman@TCLIN53.TSS.PILSHIP.COM>
2023-05-11 14:59:35 +0530

    Commit with Log4j concepts

--- src/test/java/LoggerExample/PropertiesConfigExample.java
+++ src/test/java/LoggerExample/PropertiesConfigExample.java
@@ -1,20 +1,21 @@
 package LoggerExample;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
-public class BasicConfiguratorExample {
-    
-     static Logger logger=Logger.getLogger(BasicConfiguratorExample.class); //Factory design pattern
-     
+
+public class PropertiesConfigExample {
+
+	static Logger logger=Logger.getLogger(PropertiesConfigExample.class);
 	public static void main(String[] args) {
 		
-		BasicConfigurator.configure();
+        PropertyConfigurator.configure("Log4j.properties");
 		logger.debug("This is for debug");
 		logger.info("This is info message");
 		logger.warn("This is warning message");
 		logger.error("This is error message");
 		logger.fatal("This is fatal message");
+
 	}
 
 }
--- src/test/java/LoggerExample/XMLConfigExample.java
+++ src/test/java/LoggerExample/XMLConfigExample.java
@@ -1,20 +1,20 @@
 package LoggerExample;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
-public class BasicConfiguratorExample {
-    
-     static Logger logger=Logger.getLogger(BasicConfiguratorExample.class); //Factory design pattern
-     
+public class XMLConfigExample {
+
+	static Logger logger=Logger.getLogger(XMLConfigExample.class);
 	public static void main(String[] args) {
 		
-		BasicConfigurator.configure();
+		DOMConfigurator.configure("log4j.xml");
 		logger.debug("This is for debug");
 		logger.info("This is info message");
 		logger.warn("This is warning message");
 		logger.error("This is error message");
 		logger.fatal("This is fatal message");
+
 	}
 
 }
